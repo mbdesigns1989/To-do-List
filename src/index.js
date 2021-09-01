41a63fe2777922012feabdf5d150819cd4578c36
mbdesigns1989 <mbdesigns1989@yahoo.com>
2021-09-01 23:36:59 +0300

    Update src/index.js src/index.html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import _ from 'lodash';
+import 'lodash';
 import './style.css';
 
+const todoList = document.getElementById('todo-list');
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const myTasks = [{
+  description: 'Understand JavaScript ES6',
+  completed: true,
+  index: 0,
+},
+{
+  description: 'Work on my social media',
+  completed: true,
+  index: 1,
+},
+{
+  description: 'Complete To Do List project',
+  completed: false,
+  index: 2,
+}];
