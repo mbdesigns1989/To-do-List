b75acd0065d08101888c0f26b4b3bbd138fee9ed
mbdesigns1989 <mbdesigns1989@yahoo.com>
2021-09-09 03:26:49 +0300

    Add Tests for Adding Tasks function

---
+++ add_remove.test.js
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+import { addTask, deleteTask } from './src/add_remove.js';
+
+describe('adding a task to the list', () => {
+  document.body.innerHTML = '<input type="text" placeholder="Add to your list..." id="inputList">';
+  const inputList = document.getElementById('inputList');
+  inputList.value = 'Mohamed Badr';
+  const myTasks = [];
+
+  test('Add two new tasks to the list', () => {
+    addTask(myTasks);
+    addTask(myTasks);
+    expect(myTasks).toHaveLength(2);
+  });
+
+  test('Check description of first task', () => {
+    expect(myTasks[0].description).toBe('Mohamed Badr');
+  });
+
+  test('Check status of first task', () => {
+    expect(myTasks[0].completed).toBe(false);
+  });
+
+  test('Check index of second task', () => {
+    expect(myTasks[1].index).toBe(2);
+  });
+});
+
